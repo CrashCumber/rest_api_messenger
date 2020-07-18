f1c1aff05f66333f682a7220e68a4e59061a44b1
ValentinaAsk <49205668+ValentinaAsk@users.noreply.github.com>
2020-07-18 08:26:20 +0300

    Delete base.py

--- test_api/base.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from db_connection.db_connection import DbClient
-
-class BaseCase:
-
-    @pytest.fixture(scope='function', autouse=True)
-    def setup(self, config):
-        self.db, self.url = config
